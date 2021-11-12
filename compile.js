c1bfeb5960e1e713a9f63fa7e3a195b294931fa5
Enes İnce <inceenes10@gmail.com>
2021-11-13 02:22:27 +0300

    Prettier and pre-commit configs added

--- compile.js
+++ compile.js
@@ -1,5 +1,5 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 import fs from "fs";
 import solc from "solc";
@@ -7,39 +7,26 @@ import solc from "solc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 
-const source = fs.readFileSync(inboxPath, "utf8")
-console.log(source);
-
-function findImports(path) {
-  if (path === 'lib.sol')
-    return {
-      contents:
-        'library L { function f() internal returns (uint) { return 7; } }'
-    };
-  else return { error: 'File not found' };
-}
+const source = fs.readFileSync(inboxPath, "utf8");
 
 const input = {
     language: "Solidity",
     sources: {
-    'Inbox.sol': {
-      content: source
-    }
-  },
-  settings: {
-    outputSelection: {
-      '*': {
-        '*': ['*']
-      }
-    }
-  }
-}
+"Inbox.sol": {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            "*": {
+                "*": ["*"],
+            },
+        },
+    },
+};
 
-let output = JSON.parse(
-  solc.compile(JSON.stringify(input), { import: findImports })
-);
+let output = JSON.parse(solc.compile(JSON.stringify(input)));
 
 console.log(output["contracts"]["Inbox.sol"]["Inbox"]);
